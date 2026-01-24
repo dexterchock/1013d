f7aea07a7407c409ed10e3762d503482a6898a60
dexterchock <122592855+dexterchock@users.noreply.github.com>
2026-01-25 02:42:25 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js,ts,jsx,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       fontFamily: {
